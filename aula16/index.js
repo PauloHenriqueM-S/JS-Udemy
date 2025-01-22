5ee60309bd1544c2436ed9942cd2ea4968493ea4
PauloHenriqueM-S <phttm0501@gmail.com>
2025-01-22 16:42:27 -0400

    Math object

---
+++ aula16/index.js
@@ -0,0 +1,25 @@
+console.log(!!(100 / 0));
+console.log(100 / 0); // cuidado com infinito
+
+
+
+// let num1 = 9;
+// console.log(num1 ** (1 / 2)); // 3
+// console.log(num1 ** 0.5); // 3
+
+
+// let num2 = Math.floor(num1); arredonda para baixo
+// let num2 = Math.ceil(num1); arredonda para cima
+// let num2 = Math.round(num1); arredonda para cima ou para baixo de acordo com o resto da divisão
+
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)); pega o maior numero na sequecia
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)); pega o menor numero na sequecia
+
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(Math.random()); // gera um número aleatório entre 0 e 1
+
+// console.log(Math.pow(2, 10)); // 1024
+// console.log(2 ** 10); // 1024
+
+
+
