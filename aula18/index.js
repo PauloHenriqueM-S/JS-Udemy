4d80c4fac88e8e7ea3f44db623b73926d6a8d3e5
PauloHenriqueM-S <phttm0501@gmail.com>
2025-02-04 11:09:45 -0400

    function basico

---
+++ aula18/index.js
@@ -0,0 +1,23 @@
+function soma(x, y) {
+  const resultado = x + y;
+  return resultado
+}
+
+const resultado = soma(4, 2);
+// const resultado = soma(5); // NaN
+// const resultado = soma('Luiz', ' Otávio'); // Luiz Otávio
+// const resultado = soma(); NaN
+// console.log(resultado);
+
+const raiz = function (n) {
+  return n ** 0.5
+}
+/* const raiz = (n) => {
+  return n ** 0.5
+  }
+*/
+// const raiz = n => n ** 0.5
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
