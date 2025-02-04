e9e63939ba480dfa239d4cad162a4a0c3077d02f
PauloHenriqueM-S <phttm0501@gmail.com>
2025-02-04 10:53:06 -0400

    Array basico

---
+++ aula17/index.js
@@ -0,0 +1,42 @@
+//            012345678910
+// const nome = 'Luiz Otávio'
+// console.log(nome[0]);
+
+//               0       1        2          3      4
+let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+alunos = 123
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+
+// console.log(alunos.slice(0, 3));
+
+// console.log(alunos[50]); // undefined existe mas não tem valor
+
+// delete alunos[1]; // Remove o elemento do array mas deixa o indice vazio
+
+// console.log(removido);
+// const removido = alunos.shift(); // Remove o primeiro elemento
+// const removido = alunos.pop(); // Remove o ultimo elemento
+
+// alunos.unshift('Luiza'); // Adiciona no inicio
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza' // Adiciona no fim
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+
+// alunos[0] = 'Eduardo' // Altera
+// alunos[3] = 'Luiza' // Adicionando
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
+
