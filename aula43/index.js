29ce0d62ece99ba6f169cdc199e1fe832d17fc30
PauloHenriqueM-S <phttm0501@gmail.com>
2025-03-20 17:09:24 -0400

    exercises with programming logic I

---
+++ aula43/index.js
@@ -0,0 +1,20 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function maior(n1, n2) {
+  if (n1 > n2) {
+    return n1;
+  } else {
+    return n2;
+  }
+}
+
+// Professor
+
+function max(x, y) {
+  if (x > y) return x;
+  return y;
+}
+
+// return x > y ? x : y
+
+const max2 = (x, y) => x > y ? x : y
