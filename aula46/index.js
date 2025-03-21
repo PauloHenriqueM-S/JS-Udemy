06e5d6aadff1dc8e59377687d683519798369b8d
PauloHenriqueM-S <phttm0501@gmail.com>
2025-03-21 17:08:50 -0400

    setInterval and setTimeout

---
+++ aula46/index.js
@@ -0,0 +1,21 @@
+function mostraHora() {
+  let data = new Date();
+  return data.toLocaleTimeString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  });
+}
+
+const timer = setInterval(function () {
+  console.log(mostraHora())
+}, 1000);
+
+setTimeout(function () {
+  clearInterval(timer);
+}, 3000);
+
+setTimeout(function () {
+  console.log('Executou o setTimeout');
+}, 5000);
