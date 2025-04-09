ab157657f6afdf81d33fded51184d5952a2438b4
PauloHenriqueM-S <phttm0501@gmail.com>
2025-04-09 11:34:59 -0400

    Immediately functions (IIFE)

---
+++ aula55/index.js
@@ -0,0 +1,20 @@
+// IIFE -> Immediately Invoked Function Expression
+function qualquerCoisa() {
+  console.log('Ola');
+}
+
+qualquerCoisa();
+
+(function (idade, peso, altura) {
+  const sobrenome = 'Miranda';
+  function criaNome(nome) {
+    return `${nome} ${sobrenome}`
+  }
+
+  function falaNome() {
+    console.log(criaNome('Luiz'));
+  }
+
+  falaNome();
+  console.log(idade, peso, altura);
+})(30, 80, 1.80);
