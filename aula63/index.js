4c7eda677df6e40207db671c4e784f3a30f097ab
PauloHenriqueM-S <phttm0501@gmail.com>
2025-04-12 12:11:06 -0400

    splice method

---
+++ aula63/index.js
@@ -0,0 +1,25 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// nomes.splice(índice atual, delete, item1, item2, item3);
+// pop
+// const removidos = nomes.splice(3, 2);
+// const removidos = nomes.splice(-2, Number.MAX_VALUE);
+// const removidos = nomes.splice(3, 0, 'Luiz');
+// const removidos = nomes.splice(3, 1, 'Luiz');
+// const removidos = nomes.splice(3, 2, 'Luiz');
+// const removidos = nomes.splice(3, 2, 'Luiz', 'Otávio');
+// const removidos = nomes.splice(-1, 1)
+
+// shift
+// const removidos = nomes.splice(0, 1)
+
+// push
+// nomes.splice(nomes.length, 1, 'Luiz', 'Otávio', 'Miranda')
+
+// unshift
+nomes.splice(0, 0, 'Luiz', 'Otávio')
+
+
+
+// console.log(nomes, removidos)
+console.log(nomes)
