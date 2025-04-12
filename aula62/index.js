5c27972da0164976a07c5d51b7160e5fd05ddf15
PauloHenriqueM-S <phttm0501@gmail.com>
2025-04-12 10:23:43 -0400

    review of the basics of arrays

---
+++ aula62/index.js
@@ -0,0 +1,27 @@
+// valor por referência
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const nomes = new Array('Eduardo', 'Maria', 'Joana');
+// String, objetos, funcões, números
+
+// nomes[2] = 'João';
+// delete nomes[2];
+
+// const novo = nomes;
+// const novo = [...nomes]; // Não altera o array original
+
+// novo.pop()
+// console.log(nomes);
+// console.log(novo);
+
+// nomes.push('Luiz');
+// nomes.unshift('Wallace');
+
+// const novo2 = nomes.slice(1, -3);
+// console.log(novo2);
+
+// const nome = 'Luiz Otávio Miranda';
+// const nomes = nome.split(' ');
+// console.log(nomes);
+const nomes = ['Luiz', 'Otávio', 'Miranda'];
+const nome = nomes.join(' ');
+console.log(nome);
