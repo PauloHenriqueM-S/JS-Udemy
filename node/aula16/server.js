0a910f3abf88b01c63b4375fbaffd69183257107
PauloHenriqueM-S <phttm0501@gmail.com>
2025-05-29 02:24:46 -0400

    Update

--- node/aula16/server.js
+++ node/aula16/server.js
@@ -1,26 +1,28 @@
 require('dotenv').config()
-const express = require('express')
+const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
 
+const mongoose = require('mongoose')
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.emit('pronto')
   })
   .catch(e => console.log(e))
 
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const flash = require('connect-flash')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 
-const routes = require('./routes')
-const path = require('path')
-const helmet = require('helmet')
-const csrf = require('csurf')
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware')
+const routes = require('./routes');
+const path = require('path');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
 app.use(helmet())
+
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 const sessionOptions = session({
