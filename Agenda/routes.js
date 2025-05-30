09e5d555c8b7969436e3da102150bc9e89aa9fbb
PauloHenriqueM-S <phttm0501@gmail.com>
2025-05-30 14:25:52 -0400

    Creating the login page/create account

--- Agenda/routes.js
+++ Agenda/routes.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController')
-const contatoController = require('./src/controllers/contatoController')
-
-// function meuMiddleware(req, res, next) {
-//   req.session = { nome: 'Luiz', sobrenome: 'Otávio' }
-//   console.log()
-//   console.log('Passei no seu middleware.')
-//   console.log()
-//   next()
-// }
+const loginController = require('./src/controllers/loginController')
 
 // Rotas da home
-route.get('/', homeController.paginaInicial)
-//   (req, res, next) => {
-//   // console.log()
-//   // console.log('Ainda estou aqui...')
-//   // console.log(`'ultimoMiddleware' Olha o que tem na req.sesion.nome ${req.session.nome}`)
-// }
-route.post('/', homeController.trataPost)
+route.get('/', homeController.index)
 
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial)
+// Rotas de login
+route.get('/login/index', loginController.index)
 
 module.exports = route
--- Agenda/src/controllers/contatoController.js
+++
@@ -1,6 +0,0 @@
-exports.paginaInicial = (req, res) => {
-  res.send(`
-    <h1>Esse é o contato</h1>
-    <a href='/'>Voltar</a>
-    `)
-}
