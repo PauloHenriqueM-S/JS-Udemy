f44958c7025acdf5981b5a6dbd8d4467f6a8e8b7
PauloHenriqueM-S <phttm0501@gmail.com>
2025-06-06 15:48:45 -0400

    Editing Contacts (Update)

--- Agenda/src/models/ContatoModel.js
+++ Agenda/src/models/ContatoModel.js
@@ -18,6 +18,13 @@ class Contato {
     this.contato = null
   }
 
+  async edit(id) {
+    if (typeof id !== 'string') return
+    this.valida()
+    if (this.errors.length > 0) return
+    this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true })
+  }
+
   static async buscaPorId(id) {
     if (typeof id !== 'string') return
 
